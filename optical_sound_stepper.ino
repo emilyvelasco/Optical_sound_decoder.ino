bdec4c23d1a5d96bd739816ec1f4282ffe09d1bd
emilyvelasco <54129995+emilyvelasco@users.noreply.github.com>
2020-12-02 18:23:45 -0800

    Update optical_sound_stepper.ino

--- optical_sound_stepper.ino
+++ optical_sound_stepper.ino
@@ -11,19 +11,19 @@ void setup() {
 }
  
 void loop() {
-  int val = analogRead(A0);
-  speed_value = abs(val-511);
-  direction_value = (val-511);
-  stepdelay = map(speed_value,512,0,1,500);
+  int val = analogRead(A0); //reads pot value
+  speed_value = abs(val-511); //sets the center position of the pot as 0 speed and the endstops as max speed
+  direction_value = (val-511); //sets center position of pot as divider between positive and negative direction
+  stepdelay = map(speed_value,512,0,1,500); //maps max speed_value to shortest step delay
 
 
-  if ( direction_value > 0 && speed_value > 50) {
+  if ( direction_value > 0 && speed_value > 50) { //turns motor forward if direction_value is positive and speed_value is above 50
      digitalWrite(dirpin, HIGH);
      digitalWrite(steppin, HIGH);
      delayMicroseconds(stepdelay);
      digitalWrite(steppin, LOW);
   }
-  else if (direction_value <= 0 && speed_value > 50) {
+  else if (direction_value <= 0 && speed_value > 50) { //turns motor backward if direction_value is negative and speed_value is above 50
     digitalWrite(dirpin, LOW);
     digitalWrite(steppin, HIGH);
     delayMicroseconds(stepdelay);
