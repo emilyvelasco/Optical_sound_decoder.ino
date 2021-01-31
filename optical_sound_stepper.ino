7e398cdc848668f6773b8e4cf44a994fdc13ff2c
emilyvelasco <54129995+emilyvelasco@users.noreply.github.com>
2021-01-31 15:26:58 -0800

    Update optical_sound_stepper.ino

--- optical_sound_stepper.ino
+++ optical_sound_stepper.ino
@@ -3,31 +3,120 @@ int dirpin = 2;
 int stepdelay;
 int speed_value;
 int direction_value;
+int forward_led = 4;
+int pause_led = 5;
+int reverse_led = 12;
+
 void setup() {
+  pinMode(forward_led, OUTPUT);
+  pinMode(pause_led, OUTPUT);
+  pinMode(reverse_led, OUTPUT);
   pinMode(steppin, OUTPUT);
   pinMode(dirpin, OUTPUT);
   digitalWrite(dirpin, LOW); // direction pin either LOW or HIGH to move in either direction.
-  Serial.begin(115200);
+  digitalWrite(forward_led, LOW);
+  digitalWrite(pause_led, LOW);
+  digitalWrite(reverse_led, LOW);
+  delay(400);
+  digitalWrite(forward_led, HIGH);
+  digitalWrite(pause_led, LOW);
+  digitalWrite(reverse_led, LOW); 
+  delay(400);
+  digitalWrite(forward_led, LOW);
+  digitalWrite(pause_led, HIGH);
+  digitalWrite(reverse_led, LOW);
+  delay(400);
+  digitalWrite(forward_led, LOW);
+  digitalWrite(pause_led, LOW);
+  digitalWrite(reverse_led, HIGH); 
+  delay(400);
+  digitalWrite(forward_led, LOW);
+  digitalWrite(pause_led, HIGH);
+  digitalWrite(reverse_led, LOW); 
+  delay(400);
+  digitalWrite(forward_led, HIGH);
+  digitalWrite(pause_led, LOW);
+  digitalWrite(reverse_led, LOW);
+  delay(400);
+  digitalWrite(forward_led, LOW);
+  digitalWrite(pause_led, HIGH);
+  digitalWrite(reverse_led, LOW);
+  delay(400);
+  digitalWrite(forward_led, LOW);
+  digitalWrite(pause_led, LOW);
+  digitalWrite(reverse_led, HIGH); 
+  delay(400);
+  digitalWrite(forward_led, LOW);
+  digitalWrite(pause_led, HIGH);
+  digitalWrite(reverse_led, LOW); 
+  delay(400);
+  digitalWrite(forward_led, HIGH);
+  digitalWrite(pause_led, LOW);
+  digitalWrite(reverse_led, LOW);
+  delay(400);
+  digitalWrite(forward_led, LOW);
+  digitalWrite(pause_led, LOW);
+  digitalWrite(reverse_led, LOW);
+  delay(100);
+  digitalWrite(forward_led, HIGH);
+  digitalWrite(pause_led, HIGH);
+  digitalWrite(reverse_led, HIGH);
+  delay(100);
+    digitalWrite(forward_led, LOW);
+  digitalWrite(pause_led, LOW);
+  digitalWrite(reverse_led, LOW);
+  delay(100);
+  digitalWrite(forward_led, HIGH);
+  digitalWrite(pause_led, HIGH);
+  digitalWrite(reverse_led, HIGH);
+  delay(100);
+  digitalWrite(forward_led, LOW);
+  digitalWrite(pause_led, LOW);
+  digitalWrite(reverse_led, LOW);
+  delay(100);
+  digitalWrite(forward_led, HIGH);
+  digitalWrite(pause_led, HIGH);
+  digitalWrite(reverse_led, HIGH);
+  delay(100);
+  digitalWrite(forward_led, LOW);
+  digitalWrite(pause_led, LOW);
+  digitalWrite(reverse_led, LOW);
+  delay(500);
+
 }
  
 void loop() {
-  int val = analogRead(A0); //reads pot value
-  speed_value = abs(val-511); //sets the center position of the pot as 0 speed and the endstops as max speed
-  direction_value = (val-511); //sets center position of pot as divider between positive and negative direction
-  stepdelay = map(speed_value,512,0,1,500); //maps max speed_value to shortest step delay
+  int val = analogRead(A7);
+  speed_value = abs(val-511);
+  direction_value = (val-511);
+  stepdelay = map(speed_value,512,50,1,500);
 
 
-  if ( direction_value > 0 && speed_value > 50) { //turns motor forward if direction_value is positive and speed_value is above 50
-     digitalWrite(dirpin, HIGH);
+
+  if ( direction_value > 0 && speed_value > 50) {
+     digitalWrite(dirpin, LOW);
      digitalWrite(steppin, HIGH);
      delayMicroseconds(stepdelay);
      digitalWrite(steppin, LOW);
+     digitalWrite(forward_led, HIGH);
+     digitalWrite(pause_led, LOW);
+     digitalWrite(reverse_led, LOW); 
   }
-  else if (direction_value <= 0 && speed_value > 50) { //turns motor backward if direction_value is negative and speed_value is above 50
-    digitalWrite(dirpin, LOW);
+  else if (direction_value <= 0 && speed_value > 50) {
+    digitalWrite(dirpin, HIGH);
     digitalWrite(steppin, HIGH);
     delayMicroseconds(stepdelay);
     digitalWrite(steppin, LOW);
+    digitalWrite(forward_led, LOW);
+    digitalWrite(pause_led, LOW);
+    digitalWrite(reverse_led, HIGH); 
+  }
+
+  else {
+
+    digitalWrite(forward_led, LOW);
+    digitalWrite(pause_led, HIGH);
+    digitalWrite(reverse_led, LOW); 
   }
  
 }
